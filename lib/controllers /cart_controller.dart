e2c68505e6fc333ea661b0b27a817e15cb3dc6f4
zakariya alzeidi <zk.alzeidi@gmail.com>
2023-01-03 12:28:03 +0100

    CartModel and controller

--- lib/controllers /cart_controller.dart
+++ lib/controllers /cart_controller.dart
@@ -1,8 +1,11 @@
 import 'package:food_delivery/data/repository/cart_repo.dart';
 import 'package:get/get.dart';
 
+import '../models/cart_model.dart';
+
 class CartController extends GetxController{
   final CartRepo cartRepo;
   CartController({required this.cartRepo});
+  Map<int, CartModel> _items= {};
 
 }
