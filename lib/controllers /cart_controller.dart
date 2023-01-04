fadb54d60c3a686defb7009d68a0638f622b52a9
zakariya alzeidi <zk.alzeidi@gmail.com>
2023-01-04 22:22:27 +0100

    CartModel and controller1

--- lib/controllers /cart_controller.dart
+++ lib/controllers /cart_controller.dart
@@ -2,10 +2,15 @@ import 'package:food_delivery/data/repository/cart_repo.dart';
 import 'package:get/get.dart';
 
 import '../models/cart_model.dart';
+import '../models/products_model.dart';
 
 class CartController extends GetxController{
   final CartRepo cartRepo;
   CartController({required this.cartRepo});
   Map<int, CartModel> _items= {};
+
+  void addItem(ProductModel product, int quantity){
+    _items.putIfAbsent(key, () => null)
+  }
 
 }
