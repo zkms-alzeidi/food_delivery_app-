3d43e0f28650f8b30b5c7e88a6ea3fc693888f0a
zkms-alzeidi <zk.alzeidi@gmail.com>
2023-07-08 22:27:40 +0200

    add Drinks Category

--- lib/data/repository/popular_product_repo.dart
+++ lib/data/repository/popular_product_repo.dart
@@ -9,6 +9,6 @@ class PopularProductRepo extends GetxService{
   PopularProductRepo({ required this.apiClient});
 
   Future<Response> getPopularProductList() async{
-    return await apiClient.getData(AppConstants.POPULAR_PRODUCT_URI);
+    return await apiClient.getData(AppConstants.DRINKS_URI);
   }
 }
--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -5,6 +5,7 @@ class AppConstants{
   static const String POPULAR_PRODUCT_URI= "/api/v1/products/popular";
   static const String UPLOAD_URL = "/uploads/";
   static const String RECOMMENDED_PRODUCT_URI= "/api/v1/products/recommended";
+  static const String DRINKS_URI= "/api/v1/products/drinks";
   static const String TOKEN = "DBtoken";
   static const String CART_LIST= "Cart-List";
   static const String CART_HISTORY_LIST= "History_List";
